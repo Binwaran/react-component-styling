0ca929f41fb792bcb693c4a7046cef01754fb049
Binwaran <binwaran.m@gmail.com>
2025-03-18 15:12:21 +0800

    Assignment

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,59 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Alert() {}
+function Alert({ type }) {
+  const alertStyles = {
+    error: css`
+      background-color: #FFCBCB;
+      color: black;
+      text-align: left;
+      padding: 16px;
+      border-radius: 8px;
+      font-size: 18px;
+      margin-bottom: 16px;
+    `,
+    warning: css`
+      background-color: #FFCAA4;
+      color: black;
+      text-align: left;
+      padding: 16px;
+      border-radius: 8px;
+      font-size: 18px;
+      margin-bottom: 16px;
+    `,
+    info: css`
+      background-color: #FFE9AE;
+      color: black;
+      text-align: left;
+      padding: 16px;
+      border-radius: 8px;
+      font-size: 18px;
+      margin-bottom: 16px;
+    `,
+    success: css`
+      background-color: #f1f7b5;
+      color: black;
+      text-align: left;
+      padding: 16px;
+      border-radius: 8px;
+      font-size: 18px;
+      margin-bottom: 16px;
+    `,
+  };
+
+  const message = {
+    error: 'This is error alert box',
+    warning: 'This is warning alert box',
+    info: 'This is info alert box',
+    success: 'This is success alert box',
+  };
+
+  return (
+    <div css={alertStyles[type]}>
+      {message[type]}
+    </div>
+  );
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,27 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+function Button({type}) {
+    const buttonPrimary = css`
+        background-color: blue;
+        color: white;
+        padding: 32px;
+        border-radius: 8px;
+        font-size: 24px;
+        margin-right: 16px;
+    `;
+    const buttonSecondary = css`
+        background-color: lightblue;
+        color: white;
+        padding: 32px;
+        border-radius: 8px;
+        font-size: 24px;
+    `;
+    return(
+        <button css={type === 'primary' ? buttonPrimary : buttonSecondary}>
+            {type === 'primary' ? 'Primary Button' : 'Secondary Button'}
+        </button>
+    )
+}
+export default Button;
 
-function Button() {}
